7a8f5dff90e9d7dcc96bf48b83304a1290f2703a
Swift <theycallmeswift@gmail.com>
2017-02-08 15:49:55 -0500

    Hello World!

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+# list of cat images
+images = [
+  "http://bit.ly/docker-cat-1",
+  "http://bit.ly/docker-cat-2",
+  "http://bit.ly/docker-cat-3"
+]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    return render_template("index.html", url=url)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
